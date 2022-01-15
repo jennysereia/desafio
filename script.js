2c118bf37fecf1a303ceb43380e0c124a8c2462f
jennysereia <jeniffersival@gmail.com>
2022-01-15 19:12:06 -0300

    Implementar funções de retirada de dados do API Rick and Morty no arquivo java script

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ const imagemTres = document.querySelector('#imagemTres');
 const nomeDoPersonagem = document.querySelector('#nome');
 const nomeDoPersonagemDois = document.querySelector('#nomeDois');
 const nomeDoPersonagemTres = document.querySelector('#nomeTres');
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.src = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+    });
+}
+
+botao.onclick = pegarPersonagem;
